ac5da39c4d727597cd0fd4b973e44ec6102c0ee8
Richard <rdlajeuenesse76@gmail.com>
2019-04-16 16:58:34 -0600

    left work

--- app.js
+++ app.js
@@ -24,15 +24,15 @@ console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercu
 
 //Spinal Tap Case
 function spinalCase(str){
-  var regex1 = /[^A-Za-z]/gm;
-  var regex2 = /[a-z{1}][A-Z{2}]/gm;
-  console.log(`From inside function non alphanumeric: ${str.search(regex1)}`);
-  console.log(`From inside function lower followed by upper: ${str.search(regex2)}`);
+  var regex1 = /[^A-Za-z]|([a-z{1}])([A-Z{2}])/gm;
+  //var regex2 = /([a-z{1}])([A-Z{2}])/gm;
+  var retStr = str.replace(regex1,'$1 $2');
+  ret
   //str.replace(regex1,'-')
   //str.replace(regex2,'work on this');
   //var lowerStr = str.toLowerCase();
   //var strArr = lowerStr.split(" ");
   //var retStr = strArr.join('-');
-  return str;
+  return retStr;
 }
 console.log(`after the function is called: ${spinalCase("thisIsSpinalTap")}`);
