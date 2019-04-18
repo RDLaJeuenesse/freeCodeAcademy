765f09891e87470eb3f078c76ebd774f1b1cbe4a
Richard LaJeuenesse <rdlajeuenesse76@gmail.com>
2019-04-18 16:47:09 -0600

    working on Pig Latin

--- app.js
+++ app.js
@@ -20,19 +20,36 @@ function whatIsInAName(collection, source){
     })
   })
 }
-console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
+//console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
 
 //Spinal Tap Case
 function spinalCase(str){
-  var regex1 = /[^A-Za-z]|([a-z{1}])([A-Z{2}])/gm;
-  //var regex2 = /([a-z{1}])([A-Z{2}])/gm;
-  var retStr = str.replace(regex1,'$1 $2');
-  ret
-  //str.replace(regex1,'-')
-  //str.replace(regex2,'work on this');
-  //var lowerStr = str.toLowerCase();
-  //var strArr = lowerStr.split(" ");
-  //var retStr = strArr.join('-');
+  var regex = /[^A-Za-z]|([a-z{1}])([A-Z{2}])/gm;
+  var retStr = str.replace(regex,'$1 $2');
+  var lowerStr = retStr.toLowerCase();
+  var strArr = lowerStr.split(' ');
+  retStr = strArr.join('-');
   return retStr;
 }
-console.log(`after the function is called: ${spinalCase("thisIsSpinalTap")}`);
+spinalCase("The_Andy_Griffith_Show");
+
+// Pig Latin
+function translatePigLatin(str){
+  var firstLets = [];
+  var regex = /[^aeiouy]/;
+  var strArr = str.split('');
+  //console.log(strArr);
+  for(let i = 0; i < strArr.length; i++){
+    if(strArr[i].match(regex)){
+      strArr.push(strArr.shift());
+      i--;
+    }else{
+      i = strArr.length;
+      strArr.push('ay');
+    }
+  }
+  str = strArr.join('');
+  console.log(str);
+  return str;
+}
+translatePigLatin('glove');
