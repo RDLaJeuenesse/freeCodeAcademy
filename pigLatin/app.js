7fa59bd542661998afa177942997d336b65c323d
Richard LaJeuenesse <rdlajeuenesse76@gmail.com>
2019-04-18 23:12:33 -0600

    still pulling

--- pigLatin/app.js
+++ pigLatin/app.js
@@ -1,15 +1,16 @@
 // Pig Latin
 function translatePigLatin(str){
+  console.log(`The first letter of the word is ${str[0]}.`);
   var regex = /[^aeiouy]/;
   var count = 0;
 
   // convert the string to an array of lower case characters
   let strArr = strPrep(str);
-  
+  // check if strArr[0] is 'y'
+  //strArr[0] === 'y' ? console.log('I start with y') : console.log('I DO NOT start with y');
   // if a word begins with 'y' move the 'y' to the end of the array
   // in pig latin y is used as a vowel only when it is not the first letter of the word
   if(strArr[0] === 'y'){
-    console.log("I start with y...");
     strArr.push(strArr.shift());
     strArr.push('ay')
     return strArr.join('');
@@ -35,9 +36,18 @@ function translatePigLatin(str){
     }
   }
 }
+function beginsWith(str){
+  console.log('I am here');
+  let strArr = Array.from(str);
+  strArr.push(strArr.shift());
+  strArr.push('ay')
+  return strArr.join('');
+}
 function strPrep(str){
   str = str.toLowerCase();
+  str[0] === 'y' ? beginsWith(str) : console.log(`I DO NOT start with y.`);
   let strArr = str.split('');
   return strArr;
 }
+
 console.log(translatePigLatin('yellow'));
