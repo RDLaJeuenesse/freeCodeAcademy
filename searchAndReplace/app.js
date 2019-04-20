a649b9f71400e12f293e597c80e79a54bc69355e
Richard LaJeuenesse <rdlajeuenesse76@gmail.com>
2019-04-20 11:28:15 -0600

    search and replace from work

--- searchAndReplace/app.js
+++ searchAndReplace/app.js
@@ -1,8 +1,11 @@
 //Search and Replace
 function myReplace(str, before, after) {
-    str = str.replace(before,after);
-    console.log(str);
-    return str;
-  }
-  
-  myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+  var caps = /[A-Z]/;
+  var capFlag = before[0].match(caps) ? true : false;
+  str = str.replace(before, after);
+  //console.log(capFlag);
+  //capFlag === true ? after = after[0].toUpperCase() : console.log('lower');
+  console.log(str);
+  return str;
+}
+myReplace("A quick brown fox jumped over the lazy dog", "Jumped", "leaped");
