4dd1adb9963cd9d7cb7538dabcc2d8d8a2fc064b
Richard LaJeuenesse <rdlajeuenesse76@gmail.com>
2019-04-19 21:57:09 -0600

    completed pig latin

--- pigLatin/app.js
+++ pigLatin/app.js
@@ -1,24 +1,22 @@
 // Pig Latin
 function translatePigLatin(str){
-  var regex = /[aeiouy]/;
+  var regex = /[aeiou]/;
   var count = 0;
   // prep the string
   str = strPrep(str);
   //create an array from the string
   let strArr = Array.from(str);
-  // the first character is a vowel
-  // return the str with 'way'
-  if(strArr[0].match(regex)){
+  // check the first character against regex and check if final character is y
+  if(strArr[0].match(regex) && strArr[strArr.length - 1] !== 'y'){
     strArr.push('way');
     return strArr.join('');
   }
   // rearrange the array until you hit a vowel or you run out of letters
   for(let i = 0; i < strArr.length; i++){
-    if(strArr[i].match(regex) && count < strArr.length){
+    if(!strArr[i].match(regex) && count < strArr.length){
       strArr.push(strArr.shift());
       i--;
       count++;
-      console.log(count);
     // return the pig latin translation
     }else{
       i = strArr.length;
