29ff0b09573c4a4b1b8344f469ded2a510af95c0
Richard LaJeuenesse <rdlajeuenesse76@gmail.com>
2019-04-20 11:41:33 -0600

    2nd run from work

--- searchAndReplace/app.js
+++ searchAndReplace/app.js
@@ -3,9 +3,10 @@ function myReplace(str, before, after) {
   var caps = /[A-Z]/;
   var capFlag = before[0].match(caps) ? true : false;
   str = str.replace(before, after);
-  //console.log(capFlag);
-  //capFlag === true ? after = after[0].toUpperCase() : console.log('lower');
+  console.log(capFlag);
+  capFlag === true ? after = after[0].toUpperCase() : console.log('lower');
+  console.log(after);
   console.log(str);
   return str;
 }
-myReplace("A quick brown fox jumped over the lazy dog", "Jumped", "leaped");
+myReplace("His name is Tom", "Tom", "john");
