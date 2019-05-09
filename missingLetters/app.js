23cb2a7c521661e7ca118025b9447fb2d3a56324
Richard LaJeuenesse <rdlajeuenesse76@gmail.com>
2019-05-09 16:14:50 -0600

    completed missing letters

--- missingLetters/app.js
+++ missingLetters/app.js
@@ -1,6 +1,14 @@
 //Missing letters
 function fearNotLetter(str) {
-  return str;
+  var alphaArray = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  var checkMe = alphaArray.splice(alphaArray.indexOf(str[0]), str.length);
+  for(let i = 0; i < str.length; i++){
+    if(str[i] !== checkMe[i]){
+      var missingLetter = checkMe[i];
+      break;
+    }
+  }
+  return missingLetter;
 }
 
-fearNotLetter("abce");
+fearNotLetter("abcdefghijklmnoprstuvwxyz");
