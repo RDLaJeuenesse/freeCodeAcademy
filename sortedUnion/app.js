b79830800c952d32fa8a6be56eaaa9d41c8c0b88
Richard LaJeuenesse <rdlajeuenesse76@gmail.com>
2019-05-12 22:19:43 -0600

    completed sorted union

--- sortedUnion/app.js
+++ sortedUnion/app.js
@@ -1,9 +1,16 @@
 //Sorted union
 function uniteUnique(...args) {
   console.log(args);
-  let arr = args.join();
+  var arr = [];
+  for(let i = 0; i < args.length; i++){
+    for(let j = 0; j < args[i].length; j++){
+      if(!arr.includes(args[i][j])){
+        arr.push(args[i][j]);
+      }
+    }
+  }
   console.log(arr);
   return arr;
 }
 
-uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]);
