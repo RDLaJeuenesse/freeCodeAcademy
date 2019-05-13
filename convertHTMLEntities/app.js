80b72a50d385c5b4de685a20b2cc2e13b6712c36
Richard LaJeuenesse <rdlajeuenesse76@gmail.com>
2019-05-13 11:52:04 -0600

    Completed html entities

--- convertHTMLEntities/app.js
+++ convertHTMLEntities/app.js
@@ -1,7 +1,33 @@
 //Convert HTML Entities
 function convertHTML(str) {
   // &colon;&rpar;
+  var regex = /&|<|>|'|"/;
+  var changeMe;
+  var strArr = Array.from(str);
+  for(let i = 0; i < strArr.length; i++){
+    if(strArr[i].match(regex)){
+      changeMe = strArr[i];
+      switch (changeMe){
+        case '&':
+        strArr[i] = '&amp;';
+        break;
+        case '<':
+        strArr[i] = '&lt;';
+        break;
+        case '>':
+        strArr[i] = '&gt;';
+        break;
+        case '\'':
+        strArr[i] = '&apos;';
+        break;
+        case '"':
+        strArr[i] = '&quot;';
+        break;
+      }
+    }
+  }
+  str = strArr.join('');
   return str;
 }
 
-convertHTML("Dolce & Gabbana");
+convertHTML("5 < '2");
