9fd966ddaa0aa92b7e21484b41c2dce587e77140
rdlajeuenesse <rdlajeuenesse76@gmail.com>
2019-05-15 21:34:49 -0600

    figuring out GCD

--- smallestCommonMultiple/app.js
+++ smallestCommonMultiple/app.js
@@ -1,12 +1,33 @@
 //smallest common multiple
 function smallestCommons(arr){
   arr.sort(sortAscending);
-  console.log(arr);
+  //console.log(arr);
   var rangeArr = findRange(arr);
-  console.log(rangeArr);
+  //console.log(rangeArr);
   return arr;
 }
 
+function greatestCommonDivisor(arr){
+  var a = arr[0];
+  var b = arr[1];
+  if(a === 0 || b === 0){
+    return 'no zeros allowed!';
+  }
+  
+  console.log(`Recieved a: ${a} b: ${b}: (a/b).toFixed(0): ${(a/b).toFixed(0)}`);
+
+  if(a > b){
+    return greatestCommonDivisor([(a-b),b]);
+  }
+  else{
+    console.log(`b%a after else: ${b%a}`);
+    a1 = b;
+    b1 = b%a;
+    console.log(`Assigned in else: a1: ${a1} b1: ${b1}`);
+    return greatestCommonDivisor([b, (b%a)]);
+  }
+  return arr;
+}
 
 function findRange(arr){
   var finalNum = arr[arr.length - 1];
@@ -22,3 +43,5 @@ function sortAscending(a,b){
 }
 
 smallestCommons([10, 2]);
+
+console.log(greatestCommonDivisor([1071,462]));
