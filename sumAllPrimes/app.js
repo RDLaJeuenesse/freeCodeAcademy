9225a87c48405c1fa0bda1e9eb447e1c22c6d01b
rdlajeuenesse <rdlajeuenesse76@gmail.com>
2019-05-15 19:54:18 -0600

    merging

--- sumAllPrimes/app.js
+++ sumAllPrimes/app.js
@@ -1,18 +1,17 @@
-//Sum all odd fibonacci numbers
-function sumFibs(num) {
+//Sum all primes
+// noprotect
+function sumPrimes(num) {
+  var nums = [];
+  primeNums = [];
   var sum = 0;
-  var fibsArr = [1,1];
-  for(let i = 0; i <= num; i++){
-    fibsArr.push(fibsArr[i] + fibsArr[i+1]);
+  const addPrimes = (acc, cv) => acc + cv;
+  for(let i = 2; i <= num; i++){
+    nums.push(i);
   }
-  for(let i = 0; i < fibsArr.length; i++){
-    while(fibsArr[i] <= num && fibsArr[i] % 2 !== 0){
-      sum += fibsArr[i];
-      i++;
-    }
-  }
-  num = sum;
+  console.log(nums);
+  num = primeNums.reduce(addPrimes);
+  console.log(num);
   return num;
 }
 
-sumFibs(75025);
+sumPrimes(25);
