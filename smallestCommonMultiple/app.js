056c58656a3399f6e462c4bb1dfa9bc408591e46
Richard LaJeuenesse <rdlajeuenesse76@gmail.com>
2019-05-16 17:02:49 -0600

    continued work on lcm issue is max call stack size

--- smallestCommonMultiple/app.js
+++ smallestCommonMultiple/app.js
@@ -1,16 +1,39 @@
 //smallest common multiple
+//noprotect
 function smallestCommons(arr){
   arr.sort(sortAscending);
-  var rangeArr = findRange(arr);
-  var gcd = greatestCommonDivisor(arr);
-  var lcm = lowestCommonMulitple(arr, gcd);
+  var count = arr[1];
+  let rangeArr = findRange(arr);
+  let gcd = greatestCommonDivisor(arr);
+  let lcm = lowestCommonMulitple(arr, gcd);
+  let overallLCM = rangeLCM(rangeArr, gcd, count);
   console.log(`
   arr: ${arr}
   rangeArr: ${rangeArr}
+  count: ${count}
   gcd: ${gcd}
   lcm: ${lcm}
+  overallLCM: ${overallLCM}
   `)
-  return arr;
+  return overallLCM;
+}
+
+function rangeLCM(arr, gcd, count){
+  for(let i = 0; i < arr.length; i++){
+    if(gcd % arr[i] === 0){
+      console.log(`gcd: ${gcd} is divisible by ${arr[i]}`);
+    }else{
+      console.log(`
+      gcd: ${gcd} is not evenly div by ${arr[i]}
+      i: ${i}
+      count: ${count}`);
+      count++;
+      gcd = count * arr[arr.length - 1];
+      console.log(gcd);
+      return rangeLCM(arr,gcd,count);
+    }
+  }
+  return gcd;
 }
 
 function lowestCommonMulitple(arr, gcd){
@@ -23,15 +46,15 @@ function greatestCommonDivisor(arr, refinedGCD){
   var b = arr[1];
   var gcd = a%b; 
 
-/*   console.log(`
+/* console.log(`
   Recieved 
   a: ${a}
   b: ${b}
   (a/b).toFixed(0): ${(a/b).toFixed(0)}
   gcd: ${a%b}
   refinedGCD: ${refinedGCD}
-  `);   */
-
+  `);   
+ */
   if(gcd > 0){
     var refinedGCD = gcd;
     return greatestCommonDivisor([b,(gcd)], refinedGCD);
@@ -53,4 +76,4 @@ function sortAscending(a,b){
   return a-b;
 }
 
-console.log(smallestCommons([1, 5]));
+console.log(smallestCommons([1,13]));
