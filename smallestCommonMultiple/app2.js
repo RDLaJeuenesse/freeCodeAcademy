3f49a28bc2ceb62d0f196f32942a964265f7a68a
Richard LaJeuenesse <rdlajeuenesse76@gmail.com>
2019-05-17 16:50:32 -0600

    continued work

--- smallestCommonMultiple/app2.js
+++ smallestCommonMultiple/app2.js
@@ -1,3 +1,44 @@
+function greatestCommonDivisor(arr, refinedGCD){
+  var gcdArr = [];
+  var lcmArr = [];
+
+  for(let i = 1; i < arr.length; i++){
+      gcd = (arr[i - 1] % arr[i]);
+      if(gcd > 0){
+        gcdArr.push(gcd);
+        console.log(`gcd of (arr[i - 1]) ${arr[i - 1]} and (arr[i]) ${arr[i]}: (${gcd})`);
+      }
+/*       console.log(`
+      arr[i - 1]: ${arr[i - 1]}
+      arr[i]: ${arr[i]}
+      arr[i - 1] % arr[i] (gcd): ${arr[i - 1] % arr[i]}
+      `); */
+      lcm = (arr[i - 1] * arr[i])/gcd;
+      console.log(`lcm: of (arr[i - 1]) ${arr[i - 1]} * (arr[i]) ${arr[i]} / (gcd) ${gcd}`);
+      lcmArr.push(lcm);
+  }
+  console.log(gcdArr);
+  console.log(lcmArr);
+  /*var a = arr[0];
+  var b = arr[1];
+  var gcd = a%b; 
+
+  console.log(`
+  Recieved greatestCommonDivisor
+  a: ${a}
+  b: ${b}
+  (a/b).toFixed(0): ${(a/b).toFixed(0)}
+  gcd: ${a%b}
+  refinedGCD: ${refinedGCD}
+  `); 
+  if(gcd > 0){
+    var refinedGCD = gcd;
+    return greatestCommonDivisor([b,gcd], refinedGCD);
+  }else{
+    return refinedGCD;
+  }  */
+}
+
 function rangeLCM(arr, gcd, count){
     console.log(arr);
     for(let i = 0; i < arr.length; i++){
@@ -22,26 +63,5 @@ function rangeLCM(arr, gcd, count){
     return gcd;
   }
 
-  function greatestCommonDivisor(arr, refinedGCD){ 
-    var a = arr[0];
-    var b = arr[1];
-    var gcd = a%b; 
-  
-    console.log(`
-    Recieved greatestCommonDivisor
-    a: ${a}
-    b: ${b}
-    (a/b).toFixed(0): ${(a/b).toFixed(0)}
-    gcd: ${a%b}
-    refinedGCD: ${refinedGCD}
-    `); 
-    if(gcd > 0){
-      var refinedGCD = gcd;
-      return greatestCommonDivisor([b,gcd], refinedGCD);
-    }else{
-      return refinedGCD;
-    } 
-  }
-
-  console.log(rangeLCM([5,4,3,2,1],1,5));
-  //console.log(greatestCommonDivisor([5,4,3,2,1]));
+  //console.log(rangeLCM([12,11,10,9,8,7,6,5,4,3,2,1],1,5));
+  console.log(greatestCommonDivisor([5,4,3,2,1]));
