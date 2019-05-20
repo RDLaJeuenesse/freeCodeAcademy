961230068bf5e82f353fe57e4cf8500b231cd1e2
Richard LaJeuenesse <rdlajeuenesse76@gmail.com>
2019-05-20 13:47:53 -0600

    still trying to figure this out

--- smallestCommonMultiple/app.js
+++ smallestCommonMultiple/app.js
@@ -2,14 +2,15 @@
 //noprotect
 function smallestCommons(arr){
   arr.sort(sortDescending);
-  console.log(arr);
+  //console.log(arr);
   //var count = arr[1];
   let rangeArr = findRange(arr);
-  console.log(rangeArr[rangeArr.length - 1]);
-  console.log(rangeArr[rangeArr.length - 2]);
+  //console.log(rangeArr[rangeArr.length - 1]);
+  //console.log(rangeArr[rangeArr.length - 2]);
   var count = arr[0];
-  console.log(rangeArr);
+  //console.log(rangeArr);
   let gcd = greatestCommonDivisor(arr);
+  console.log(gcd);
   let lcm = lowestCommonMulitple(arr, gcd);
   let overallLCM = rangeLCM(rangeArr, gcd, count);
 /*   console.log(`
@@ -24,20 +25,20 @@ function smallestCommons(arr){
 }
 //noprotect
 function rangeLCM(arr, gcd, count){
-  console.log(arr);
+  //console.log(arr);
   for(let i = 0; i < arr.length; i++){
     if(gcd % arr[i] === 0){
-      console.log(`
+      /* console.log(`
       if executes for mod = 0
       gcd: ${gcd} evenly divisible by arr[i]: ${arr[i]}
-      count: ${count}`);
+      count: ${count}`); */
     }else{
-      console.log(`
+     /*  console.log(`
       else exicutes to rerun rangeLCM when mod = 0 is not possible
       gcd: ${gcd} is not evenly div by ${arr[i]}
       i: ${i}
       count: ${count}
-      `);
+      `); */
       count++;
       gcd = count * arr[0];
       //noprotect
@@ -74,20 +75,17 @@ function greatestCommonDivisor(arr, refinedGCD){
 }
 
 function findRange(arr){
-  console.log(arr);
   var finalNum = arr[0];
   var arr2 = [];
   for(let i = finalNum; i <= finalNum && i > 0; i--){
-    console.log(i);
   arr2.push(i);
   }
-  console.log(arr2);
   return arr2;
 }
 
 function sortDescending(a,b){
   return b-a;
 }
-//console.log(smallestCommons([1,5]));
+console.log(smallestCommons([6,2]));
 //console.log(greatestCommonDivisor([1,13]));
-console.log(rangeLCM([5,4,3,2,1],1,5));
+//console.log(rangeLCM([5,4,3,2,1],1,5));
