6d27227022fad8f36dd0db44d27557ca4299f8a9
Richard LaJeuenesse <rdlajeuenesse76@gmail.com>
2019-05-21 16:21:09 -0600

    still trying to stop the stack size issue

--- smallestCommonMultiple/app.js
+++ smallestCommonMultiple/app.js
@@ -5,12 +5,12 @@ function smallestCommons(arr){
   //console.log(arr);
   //var count = arr[1];
   let rangeArr = findRange(arr);
-  //console.log(rangeArr[rangeArr.length - 1]);
-  //console.log(rangeArr[rangeArr.length - 2]);
-  var count = findCountStart(rangeArr);
+  console.log(rangeArr);
+  var count = Math.pow(arr[0],findCountStart(rangeArr[0]));
+  console.log(`count: ${count}`);
   //console.log(rangeArr);
   let gcd = greatestCommonDivisor(arr);
-  console.log(gcd);
+  //console.log(gcd);
   let lcm = lowestCommonMulitple(arr, gcd);
   let overallLCM = rangeLCM(rangeArr, gcd, count);
 /*   console.log(`
@@ -27,19 +27,19 @@ function smallestCommons(arr){
 function rangeLCM(arr, gcd, count){
   for(let i = 0; i < arr.length; i++){
     if(gcd % arr[i] === 0){
-      /* console.log(`
+      console.log(`
       if executes for mod = 0
       gcd: ${gcd} evenly divisible by arr[i]: ${arr[i]}
-      count: ${count}`); */
+      count: ${count}`);
     }else{
-     /*  console.log(`
+     console.log(`
       else exicutes to rerun rangeLCM when mod = 0 is not possible
       gcd: ${gcd} is not evenly div by ${arr[i]}
       i: ${i}
       count: ${count}
-      `); */
-      count++;
-      gcd = count * arr[0];
+      `);
+      count = count + arr[0];
+      gcd = count + arr[0];
       //noprotect
       return rangeLCM(arr,gcd,count);
     }
@@ -57,14 +57,14 @@ function greatestCommonDivisor(arr,innerCount){
   var b = arr[1];
   var rem = a%b; 
   //console.log(gcd);
-  /*console.log(`
+/*   console.log(`
   Recieved greatestCommonDivisor
   a: ${a}
   b: ${b}
   (a/b).toFixed(0): ${(a/b).toFixed(0)}
-  gcd: ${a%b}
-  refinedGCD: ${refinedGCD}
-  `); */
+  rem: ${a%b}
+  innerCount: ${innerCount}
+  `);  */
   if(rem === 0){
     return b;
   }else{
@@ -74,23 +74,30 @@ function greatestCommonDivisor(arr,innerCount){
     return greatestCommonDivisor([a,b]);
   }
 }
+function sumPrimes(num) {
+  var primeArray = [2];
+  for(let i = 2; i <= num; i++){
+    if(isPrime(i)){
+      primeArray.push(i);
+    }
+  };
+  return primeArray.length;
+}
 
-function findCountStart(arr){
+function isPrime(num){
   var amIPrime;
-  var count = 0;
-  arr.forEach(function(element){
-    for(let i = 2; i <= element; i++){
-      if(element % i === 0){
-        console.log(`amIPrime: ${amIPrime}`);
-        amIPrime = false;
-      }else{
-        amIPrime = true;
-        console.log(amIPrime);
-        count++;
-        console.log(`count: ${count}`);
-      }
-    };
-  });
+  for(let i = 2; i < num; i++){
+    if(num % i === 0){
+      return amIPrime = false;
+    }else{
+      amIPrime = true;
+    }
+  }
+  return amIPrime;
+}
+function findCountStart(num){
+  var primeCount = sumPrimes(num);
+  return primeCount - 2;
 }
 
 function findRange(arr){
@@ -106,7 +113,7 @@ function sortDescending(a,b){
   return b-a;
 }
 
-console.log(findCountStart([1,2,3,4,5]));
-//console.log(smallestCommons([1,5]));
+//console.log(findCountStart([13]));
+console.log(smallestCommons([1,13]));
 //console.log(greatestCommonDivisor([1,13]));
 //console.log(rangeLCM([5,4,3,2,1],1,5));
