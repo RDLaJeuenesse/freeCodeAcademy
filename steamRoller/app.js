eb0103ffc4582775aff1a95d5c103b40349d7e66
Richard LaJeuenesse <rdlajeuenesse76@gmail.com>
2019-05-28 15:58:54 -0600

    completed steamRoller

--- steamRoller/app.js
+++ steamRoller/app.js
@@ -1,11 +1,7 @@
 //Spinal Tap Case
-function spinalCase(str){
-  var regex = /[^A-Za-z]|([a-z{1}])([A-Z{2}])/gm;
-  var retStr = str.replace(regex,'$1 $2');
-  var lowerStr = retStr.toLowerCase();
-  var strArr = lowerStr.split(' ');
-  retStr = strArr.join('-');
-  return retStr;
+function steamrollArray(arr) {
+  return arr.flat(arr.length);
 }
-console.log(spinalCase("The_Andy_Griffith_Show"));
+
+console.log(steamrollArray([1, {}, [3, [[4]]]]));
 
