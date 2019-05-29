8f0a84ddb4733fb2703ee3e0b0318c13b6cd84fd
Richard LaJeuenesse <rdlajeuenesse76@gmail.com>
2019-05-29 16:30:49 -0600

    working on argumentsOptional

--- argumentsOptional/app.js
+++ argumentsOptional/app.js
@@ -1,6 +1,13 @@
 //argumentsOptional
-function addTogether() {
+function addTogether(...args) {
+  for(let i = 0; i < args.length; i++){
+    console.log(args[i]);
+    if(args[i] !== Number()){
+      console.log(`not a number: ${args[i]}`)
+      return undefined;
+    }
+  }
   return false;
 }
 
-addTogether(2,3);
+console.log(addTogether("http://bit.ly/IqT6zt"));
