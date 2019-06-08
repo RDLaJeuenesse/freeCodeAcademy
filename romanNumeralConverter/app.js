0c650e13334f18d346c09075587911b7afa07d6e
rdlajeuenesse <rdlajeuenesse76@gmail.com>
2019-06-08 15:01:24 -0600

    complete rewrite of romanNumeralConverter

--- romanNumeralConverter/app.js
+++ romanNumeralConverter/app.js
@@ -2,30 +2,29 @@ function convertToRoman(num) {
 
     var romNum = ['I','IV','V','IX','X','XL','L','XC','C','CD','D','CM','M'];
     var decNum = [1,4,5,9,10,40,50,90,100,400,500,900,1000];
-    var pickle = decNum.map(function(cv, i, decNum){
-        if(num >= decNum[i]){
-            return decNum[i];
-        }
-    })
-    for(let i = 0; i < pickle.length; i++){
-        if(pickle[i] === undefined){
-            pickle.pop();
-            i--;
-        }
-    }
-    var start = pickle.reduce(function(acc,curV){
-        return Math.min(num,curV);
-    })
+    var deciStart = decNum.map(
+        function(cv, i, decNum){
+            if(num >= decNum[i]){
+                return decNum[i];}
+        }).filter(function(el){
+            return el !== undefined;
+        }).reduce(function(acc,curV){
+            return Math.min(num,curV);
+        });
+    var romStart = romNum[decNum.indexOf(deciStart)];
     var romStr = 'UNUSED';
+    var deciConvertArr = ['fill me'];
+    pickle(num, deciStart, romStart);
+    function pickle(seed, deciStart, romStart){
 
-    console.log(`
-    num: ${num}
-    decNum: ${decNum}
-    start: ${start}
-    pickle: ${pickle}
-    pickle.length: ${pickle.length}
-    romStr: ${romStr}
-    `)
+        console.log(`
+        num: ${num}
+        deciStart: ${deciStart}
+        romStart: ${romStart}
+        romStr: ${romStr}
+        deciConvertArr = ${deciConvertArr}
+        `);
+    } 
 }
 
    
