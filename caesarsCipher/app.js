9d2ffe4528793733391e4eced2e4d649536de660
Richard LaJeuenesse <rdlajeuenesse76@gmail.com>
2019-06-11 14:50:44 -0600

    continued work on ceasar cypher

--- caesarsCipher/app.js
+++ caesarsCipher/app.js
@@ -1,17 +1,27 @@
 function rot13(str) { // LBH QVQ VG!
-  var regex = /[A-Z0-9\s]/
+  var regex1 = /[A-Z]/;
+  var regex2 = /[0-9]/;
   var decoded = '';
+  console.log(str);
+  console.log(str.split('').map(function(cv, ind){
+    if(cv.match(regex1)){
+      return (str.charCodeAt(ind));
+    }else{
+      return(32);
+    }
+  }));
   str = str.split('').map(function(cv,ind){
-    if(cv.match(regex)){
+    if(cv.match(regex1)){
       return (str.charCodeAt(ind) - 13);
     }
   })
-  console.log(`
-  str: ${str}
-  decoded: ${decoded}
-  `)
-    return str;
+  console.log(str);
+  for(let i = 0; i < str.length; i++){
+    decoded = decoded.concat((String.fromCharCode(str[i])));
   }
+  console.log(decoded);
+  console.log(typeof(decoded));
+}
   
   // Change the inputs below to test
   rot13("SERR PBQR PNZC");
