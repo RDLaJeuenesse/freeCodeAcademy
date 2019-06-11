8f8f92daedd8ae6e2c676243904ad8e89059b011
Richard LaJeuenesse <rdlajeuenesse76@gmail.com>
2019-06-11 16:09:06 -0600

    completed ceaser cypher

--- caesarsCipher/app.js
+++ caesarsCipher/app.js
@@ -3,7 +3,10 @@ function rot13(str) { // LBH QVQ VG!
   var decoded = '';
   str = str.split('').map(function(cv,ind){
     if(cv.match(regex)){
-      return (str.charCodeAt(ind) - 13);
+      var jumpCheck = str.charCodeAt(ind) - 13 >= 65;
+      return (jumpCheck ? str.charCodeAt(ind) - 13 : str.charCodeAt(ind) + 13);
+    }else{
+      return str.charCodeAt(ind);
     }
   })
   for(let i = 0; i < str.length; i++){
@@ -13,4 +16,4 @@ function rot13(str) { // LBH QVQ VG!
 }
   
   // Change the inputs below to test
-  console.log(rot13("SERR PBQR PNZC"));
+  console.log(rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT."));
