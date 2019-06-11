b9b4250f5bbdd81c93c94f6ae721ef1bd5a5f17e
rdlajeuenesse <rdlajeuenesse76@gmail.com>
2019-06-11 07:43:09 -0600

    working on ceaser cypher

--- caesarsCipher/app.js
+++ caesarsCipher/app.js
@@ -1,5 +1,15 @@
 function rot13(str) { // LBH QVQ VG!
-  
+  var regex = /[A-Z0-9\s]/
+  var decoded = '';
+  str = str.split('').map(function(cv,ind){
+    if(cv.match(regex)){
+      return (str.charCodeAt(ind) - 13);
+    }
+  })
+  console.log(`
+  str: ${str}
+  decoded: ${decoded}
+  `)
     return str;
   }
   
