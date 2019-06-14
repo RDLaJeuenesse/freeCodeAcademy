a2d4dae84355410ac56156c4cd1f127f9ebb4d7e
Richard LaJeuenesse <rdlajeuenesse76@gmail.com>
2019-06-14 13:59:33 -0600

    completed phoneNumberValidator start cashRegister

--- telephoneNumberValidation/app.js
+++ telephoneNumberValidation/app.js
@@ -1,6 +1,11 @@
 function telephoneCheck(str) {
   // Good luck!
-  return true;
+  var regEx = /((^1\s?\-?\(\d{3}\))|(^1\s\d{3})|(^\(\d{3}\)|(^\d{3})))(\-?\s?)(\d{3})(\s?\-?\d{4})$/;
+  if(str.match(regEx)){
+    return true;
+  }else{
+    return false;
+  }
 }
 
-telephoneCheck("555-555-5555");
+console.log(telephoneCheck("555-555-5555"));
