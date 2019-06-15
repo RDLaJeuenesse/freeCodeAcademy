18b727d7b71279730cd6c41de5dd0f4638ab13b8
Richard LaJeuenesse <rdlajeuenesse76@gmail.com>
2019-06-15 10:11:40 -0600

    add notes

--- cashRegister/app.js
+++ cashRegister/app.js
@@ -1,10 +1,17 @@
 function checkCashRegister(price, cash, cid) {
   var change = 'CALCULATE CHANGE I SHOULD BE AN ARRAY';
   // Here is your change, ma'am.
-  var pickle = {};
-  pickle.status = 'I NEED A STATUS';
-  pickle.change = change;
-  console.log(pickle);
+  //THREE STATUS RETURNS
+  //IF THE DRAWER DOES NOT CONTAIN ENOUGH MONEY
+    //STATUS: "INSUFFICIENT_FUNDS", CHANGE[]
+  //IF THE CHANGE AND DRAWER ARE ===
+    //STATUS: "CLOSED", CHANGE[...]
+  //IF DRAWER > CHANGE
+    //STATUS: "OPEN", CHANGE[...]
+  var tinder = {};
+  tinder.status = 'I NEED A STATUS';
+  tinder.change = change;
+  console.log(tinder);
   return change;
 }
 
@@ -19,4 +26,4 @@ function checkCashRegister(price, cash, cid) {
 // ["TWENTY", 60],
 // ["ONE HUNDRED", 100]]
 
-checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
