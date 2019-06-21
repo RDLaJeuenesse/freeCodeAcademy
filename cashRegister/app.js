39c8836a06038295cd6301962b0f78a7a5db357b
Richard LaJeuenesse <rdlajeuenesse76@gmail.com>
2019-06-21 11:06:31 -0600

    still working on cashRegister

--- cashRegister/app.js
+++ cashRegister/app.js
@@ -1,5 +1,6 @@
 function checkCashRegister(price, cash, cid) {
   var change = cash - price;
+  console.log(change);
   var tinder = {};
   var totalCID = cid.map(function(value, index){
     return value[1];
@@ -7,18 +8,24 @@ function checkCashRegister(price, cash, cid) {
     return a + b;
   }).toFixed(2);
   console.log(totalCID);
-  // Here is your change, ma'am.
-  //THREE STATUS RETURNS
-  //IF THE DRAWER DOES NOT CONTAIN ENOUGH MONEY
-    //STATUS: "INSUFFICIENT_FUNDS", CHANGE[]
-  //IF THE CHANGE AND DRAWER ARE ===
-    //STATUS: "CLOSED", CHANGE[...]
-  //IF DRAWER > CHANGE
-    //STATUS: "OPEN", CHANGE[...]
-  change > 0 ? (tinder.change = change, tinder.status = "THE CHANGE DUE") : (tinder.change = change, tinder.status = "YOU OWE ME MONEY");
-  if(change === 0){
-    tinder.status = "WE ARE EVEN";
+  var caseFlag = change < totalCID;
+  console.log(caseFlag);
+  switch(caseFlag){
+    case (true):
+      console.log('we need to give change.');
+      break;
+    case (false):
+      console.log('insuffiecient funds to make change');
+      break;
+    default:
+      console.log('change and till are equal.')
+      break;
   }
+/*   totalCID > change ? (tinder.change = cid, tinder.status = "OPEN") : (tinder.change = [], tinder.status = "INSUFFIECIENT_FUNDS");
+  if(change === totalCID){
+    tinder.status = "CLOSED";
+    tinder.change = cid;
+  } */
   return tinder;
 }
 
@@ -33,4 +40,4 @@ function checkCashRegister(price, cash, cid) {
 // ["TWENTY", 60],
 // ["ONE HUNDRED", 100]]
 
-console.log(checkCashRegister(19.5, 32.5, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+console.log(checkCashRegister(19.5, 19.5, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
