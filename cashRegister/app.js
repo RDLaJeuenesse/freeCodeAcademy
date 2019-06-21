c7c5a743b0ef7777e3c722b65b9366a037dddd25
rdlajeuenesse <rdlajeuenesse76@gmail.com>
2019-06-21 07:48:14 -0600

    working on cash

--- cashRegister/app.js
+++ cashRegister/app.js
@@ -1,5 +1,12 @@
 function checkCashRegister(price, cash, cid) {
-  var change = 'CALCULATE CHANGE I SHOULD BE AN ARRAY';
+  var change = cash - price;
+  var tinder = {};
+  var totalCID = cid.map(function(value, index){
+    return value[1];
+  }).reduce(function(a,b){
+    return a + b;
+  }).toFixed(2);
+  console.log(totalCID);
   // Here is your change, ma'am.
   //THREE STATUS RETURNS
   //IF THE DRAWER DOES NOT CONTAIN ENOUGH MONEY
@@ -8,11 +15,11 @@ function checkCashRegister(price, cash, cid) {
     //STATUS: "CLOSED", CHANGE[...]
   //IF DRAWER > CHANGE
     //STATUS: "OPEN", CHANGE[...]
-  var tinder = {};
-  tinder.status = 'I NEED A STATUS';
-  tinder.change = change;
-  console.log(tinder);
-  return change;
+  change > 0 ? (tinder.change = change, tinder.status = "THE CHANGE DUE") : (tinder.change = change, tinder.status = "YOU OWE ME MONEY");
+  if(change === 0){
+    tinder.status = "WE ARE EVEN";
+  }
+  return tinder;
 }
 
 // Example cash-in-drawer array:
@@ -26,4 +33,4 @@ function checkCashRegister(price, cash, cid) {
 // ["TWENTY", 60],
 // ["ONE HUNDRED", 100]]
 
-console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+console.log(checkCashRegister(19.5, 32.5, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
